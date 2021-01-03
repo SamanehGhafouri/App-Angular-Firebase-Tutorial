3777e895d43ee252666a15b604132a894062ceba
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-03 17:11:54 -0500

    Improving optimistic updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,16 @@ import {CdkDragDrop, transferArrayItem} from "@angular/cdk/drag-drop";
 import {MatDialog} from "@angular/material/dialog";
 import {TaskDialogComponent} from "./task-dialog/task-dialog.component";
 import {TaskDialogResult} from "./task-dialog/task-dialog.component";
-import {AngularFirestore} from "@angular/fire/firestore";
+import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
+import {BehaviorSubject} from "rxjs";
 
+const getObservable = (collection: AngularFirestoreCollection<Task>) => {
+  const subject = new BehaviorSubject([]);
+  collection.valueChanges({idField: 'id'}).subscribe((val: Task[]) => {
+    subject.next(val);
+  });
+  return subject;
+};
 
 @Component({
   selector: 'app-root',
@@ -13,21 +21,9 @@ import {AngularFirestore} from "@angular/fire/firestore";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  todo = this.store.collection('todo').valueChanges({ idField: 'id'});
-  inProgress = this.store.collection('inprogress').valueChanges({ idField: 'id'});
-  done = this.store.collection('done').valueChanges({idField: 'id'});
-  // todo: Task[] = [
-  //   {
-  //     title: 'Buy milk',
-  //     description: 'Go to the store and buy milk'
-  //   },
-  //   {
-  //     title: 'Create an app',
-  //     description: 'Using Firebase and Angular create an app!'
-  //   }
-  // ];
-  // inProgress: Task[] =[];
-  // done: Task[] = [];
+  todo = getObservable(this.store.collection('todo'));
+  inProgress = getObservable(this.store.collection('inProgress'));
+  done = getObservable(this.store.collection('done'));
 
   editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void{
     const dialogRef = this.dialog.open(TaskDialogComponent, {
@@ -38,11 +34,8 @@ export class AppComponent {
       },
     });
     dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
-      // const dataList = this[list];
-      // const taskIndex = dataList.indexOf(task);
       if (result.delete){
         this.store.collection(list).doc(task.id).delete();
-        // dataList.splice(taskIndex, 1);
       }
       else {
         this.store.collection(list).doc(task.id).update(task);
@@ -50,8 +43,8 @@ export class AppComponent {
     });
   }
 
-  drop(event: CdkDragDrop<Task[]>): void{
-    if(event.previousContainer === event.container){
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
       return;
     }
     const item = event.previousContainer.data[event.previousIndex];
@@ -62,7 +55,6 @@ export class AppComponent {
       ]);
       return promise;
     });
-
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
@@ -80,7 +72,6 @@ export class AppComponent {
     });
     dialogRef
       .afterClosed()
-      // .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
       .subscribe((result: TaskDialogResult) => this.store.collection('todo').add(result.task));
   }
 }
