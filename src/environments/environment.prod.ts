79fb68c54c51c746f1ba96ea4a5856edd09fc355
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-03 17:18:11 -0500

    deployed the app and the url is https://matfire-35afc.web.app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-A-KGopnnzYI7zlUcSFrcoUjntFQKv7c",
+    authDomain: "matfire-35afc.firebaseapp.com",
+    projectId: "matfire-35afc",
+    storageBucket: "matfire-35afc.appspot.com",
+    messagingSenderId: "59597544404",
+    appId: "1:59597544404:web:ffe4482e9535ff38925441",
+    measurementId: "G-LTRP9KSB7V"
+  }
 };
