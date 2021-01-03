05f3378f780aee2d94885827fa9e7e316f32d2a5
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-03 15:46:24 -0500

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC-A-KGopnnzYI7zlUcSFrcoUjntFQKv7c",
+    authDomain: "matfire-35afc.firebaseapp.com",
+    projectId: "matfire-35afc",
+    storageBucket: "matfire-35afc.appspot.com",
+    messagingSenderId: "59597544404",
+    appId: "1:59597544404:web:ffe4482e9535ff38925441",
+    measurementId: "G-LTRP9KSB7V"
+  }
 };
 
 /*
